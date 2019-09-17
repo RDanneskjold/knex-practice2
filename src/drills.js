6e87b5caed0dfa6d5402e9c6039d9353333d8310
Ben Tilghman <ben.tilghman@gmail.com>
2019-09-16 23:31:46 -0400

    practice and drills done

---
+++ src/drills.js
@@ -0,0 +1,54 @@
+require('dotenv').config();
+const knex = require('knex');
+
+const knexInstance = knex({
+  client: 'pg',
+  connection: process.env.DB_URL,
+})
+
+function search(searchTerm) {
+  knexInstance
+    .select('*')
+    .from('shopping_list')
+    .where('name', 'ILIKE', `%${searchTerm}%`)
+    .then( results => {
+      console.log(results)
+    })
+}
+
+function paginate(pageNumber) {
+  const resultsPerPage = 5;
+  const offset = resultsPerPage * (pageNumber - 1);
+
+  knexInstance
+    .select('*')
+    .from('shopping_list')
+    .limit(resultsPerPage)
+    .offset(offset)
+    .then( results => {
+      console.log(results)
+    }) 
+}
+
+function addedAfter(daysAgo) {
+  knexInstance
+    .select('*')
+    .from('shopping_list')
+    .where('date_added', '>', knexInstance.raw(`now() - '?? days'::INTERVAL`, daysAgo))
+    .then( results => {
+      console.log(results)
+    })
+}
+
+function totalCost() {
+  knexInstance
+    .select('category')
+    .from('shopping_list')
+    .groupBy('category')
+    .sum('price AS total')
+    .then( results => {
+      console.log(results)
+    })
+}
+
+addedAfter(30);
---
+++ src/practice.js
@@ -0,0 +1,60 @@
+require('dotenv').config()
+const knex = require('knex')
+
+const knexInstance = knex({
+  client: 'pg',
+  connection: process.env.DB_URL
+})
+
+function searchByProduceName(searchTerm) {
+  knexInstance
+    .select('product_id', 'name', 'price', 'category')
+    .from('amazong_products')
+    .where( 'name', 'ILIKE', `%${searchTerm}%` )
+    .then( result => {
+      console.log(result)
+    })
+}
+
+function paginateProducts(page) {
+  const numProducts = 10;
+  const offset = numProducts * (page - 1);
+
+  knexInstance
+    .select('product_id', 'name', 'price', 'category')
+    .from('amazong_products')
+    .limit(numProducts)
+    .offset(offset)
+    .then(result => {
+      console.log(result)
+    })
+}
+
+function getProductsWithImages() {
+  knexInstance
+    .select('product_id', 'name', 'price', 'category')
+    .from('amazong_products')
+    .whereNotNull('image')
+    .then(result => {
+      console.log(result)
+      console.log('There are', result.length, 'results.')
+    })
+}
+
+function mostPopularVideos(days) {
+  knexInstance
+    .select('video_name', 'region')
+    .count('date_viewed AS views')
+    .where('date_viewed', '>', knexInstance.raw(`now() - '?? days'::INTERVAL`, days))
+    .from('whopipe_video_views')
+    .groupBy('video_name', 'region')
+    .orderBy([
+      { column: 'region', order: 'ASC' },
+      { column: 'views', order: 'DESC' },
+    ])
+    .then(result => {
+      console.log(result)
+    })
+  }
+
+mostPopularVideos(30);
